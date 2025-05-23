a0e05b77c7ee739f9c9d024c726845f70b0a9b47
ahmdhk9 <ahmdhk9@users.noreply.github.com>
2025-05-23 14:26:40 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 from flask import Flask
+import requests
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "🚀 OmniMindUltimate يعمل بنجاح!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-=======
-
->>>>>>> 78ca8ea80d0ef91324596946ad60aca6b6e73da3
+@app.route("/")
+def hello():
+    return "Hello from Render!"
